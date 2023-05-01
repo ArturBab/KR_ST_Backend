96bd9b50b43207a68008231f8e6b2cb11b78cd7c
ArturBab <90608148+ArturBab@users.noreply.github.com>
2023-05-01 14:11:40 +0300

    Update urls.py

--- KR_BACKEND/urls.py
+++ KR_BACKEND/urls.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 from krBackend.views import *
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="KR ST BACKEND SWAGGER METHODS DOC",
+      default_version='1.0.0',
+      description="Swagger KR Backend doc",
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +25,5 @@ urlpatterns = [
     path('api/users/', UserAPIList.as_view()),
     path('api/auth', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
