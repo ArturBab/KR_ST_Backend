665f91fe52e540bf87f53d51b7474bf728f1498d
ArturBab <90608148+ArturBab@users.noreply.github.com>
2023-05-15 12:14:59 +0300

    Update permissions.py

--- krBackend/permissions.py
+++ krBackend/permissions.py
@@ -2,15 +2,12 @@ from rest_framework import permissions
 from krBackend.models import *
 
 
-class IsAuthorUpdate(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method is permissions.SAFE_METHODS:
+class IsAuthorUpdateOnly(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
             return True
 
-        if request.user.is_authenticated and request.method == 'GET':
-            return bool(request.user and request.user.is_authenticated)
-        else:
-            return bool(request.user.is_authenticated and User.objects.filter(pk=request.user.id, groups__name='Author').exists())
+        return obj.user == request.user
 
 
 class IsUserOrReadOnly(permissions.BasePermission):
